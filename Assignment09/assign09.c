65c8546fcbf98428a8a3cc899cbf4055465515b1
eunsoo <ehfl0489@gmail.com>
2025-03-20 17:34:00 +0900

    push

--- Assignment09/assign09.c
+++ Assignment09/assign09.c
@@ -7,13 +7,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main(void)
+void HexToint(void) 
 {
 	int num;
-	
+
 	printf("16진수 정수?");
 	scanf("%x", &num);
 	printf("16진수 %x는 10진수로 %d입니다", num, num);
 
+}
+
+int main(void)
+{
+	HexToint();
 	return 0;
 }
