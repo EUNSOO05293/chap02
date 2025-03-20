b7d66215a2c1fde5d318b123efff346d6900b5a6
eunsoo <ehfl0489@gmail.com>
2025-03-20 17:28:18 +0900

    Assignmnet09 추가

---
+++ Assignment09/assign09.c
@@ -0,0 +1,19 @@
+/* 파일명 Assignment-09.c
+ * 작성자: 이은수
+ * 날짜: 2025.3.20
+  버전: v1.0
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+	
+	printf("16진수 정수?");
+	scanf("%x", &num);
+	printf("16진수 %x는 10진수로 %d입니다", num, num);
+
+	return 0;
+}
