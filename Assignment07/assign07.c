41fd3a4604f66f5ef466879ed0bfe9020452dcf9
eunsoo <ehfl0489@gmail.com>
2025-03-20 17:11:04 +0900

    push

--- Assignment07/assign07.c
+++ Assignment07/assign07.c
@@ -1,6 +1,25 @@
+/* 파일명 Assignment-07.c
+ * 작성자: 이은수
+ * 날짜: 2025.3.20
+  버전: v1.0
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-int main() 
+
+void CoffeeOrder(void) 
 {
+    char coffee;
+    int size;
 
+    printf("커피 사이즈(S,T,G)와 주문 수량? ");
+    scanf(" %c %d", &coffee, &size);
+    printf("%c 사이즈 %d잔을 주문합니다", coffee, size);
 
+}
+
+int main(void)
+{
+    CoffeeOrder();
+    return 0;
 }
